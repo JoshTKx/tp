4b55e87be7036a552a599c062535ceacf3432445
Joshua  Teo <Joshuakxteo@gmail.com>
2026-03-21 20:47:02 +0800

    Add EditMenuCommand and parser

---
+++ src/main/java/seedu/homechef/logic/commands/EditMenuCommand.java
@@ -0,0 +1,216 @@
+package seedu.homechef.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.homechef.logic.parser.CliSyntax.PREFIX_AVAILABILITY;
+import static seedu.homechef.logic.parser.CliSyntax.PREFIX_MENU_NAME;
+import static seedu.homechef.logic.parser.CliSyntax.PREFIX_PRICE;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import seedu.homechef.commons.core.index.Index;
+import seedu.homechef.commons.util.CollectionUtil;
+import seedu.homechef.commons.util.ToStringBuilder;
+import seedu.homechef.logic.Messages;
+import seedu.homechef.logic.commands.exceptions.CommandException;
+import seedu.homechef.model.Model;
+import seedu.homechef.model.menu.MenuItem;
+import seedu.homechef.model.menu.MenuItemName;
+import seedu.homechef.model.menu.Price;
+
+/**
+ * Edits the details of an existing menu item.
+ */
+public class EditMenuCommand extends Command {
+
+    public static final String COMMAND_WORD = "edit-menu";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the menu item identified "
+            + "by the index number used in the displayed menu list. "
+            + "At least one field must be provided.\n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + "[" + PREFIX_MENU_NAME + "NAME] "
+            + "[" + PREFIX_PRICE + "PRICE] "
+            + "[" + PREFIX_AVAILABILITY + "AVAILABILITY]\n"
+            + "Example: " + COMMAND_WORD + " 1 " + PREFIX_PRICE + "6.00 " + PREFIX_AVAILABILITY + "false";
+
+    public static final String MESSAGE_EDIT_MENU_ITEM_SUCCESS = "Edited menu item: %1$s $%2$s (available: %3$s)";
+    public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
+    public static final String MESSAGE_DUPLICATE_MENU_ITEM =
+            "A menu item with this name already exists in the menu";
+
+    private final Index index;
+    private final EditMenuDescriptor editMenuDescriptor;
+
+    /**
+     * @param index of the menu item in the filtered menu list to edit
+     * @param editMenuDescriptor details to edit the menu item with
+     */
+    public EditMenuCommand(Index index, EditMenuDescriptor editMenuDescriptor) {
+        requireNonNull(index);
+        requireNonNull(editMenuDescriptor);
+        this.index = index;
+        this.editMenuDescriptor = new EditMenuDescriptor(editMenuDescriptor);
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<MenuItem> lastShownList = model.getFilteredMenuItemList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_MENU_ITEM_DISPLAYED_INDEX);
+        }
+
+        MenuItem menuItemToEdit = lastShownList.get(index.getZeroBased());
+        MenuItem editedMenuItem = createEditedMenuItem(menuItemToEdit, editMenuDescriptor);
+
+        if (!menuItemToEdit.isSameMenuItem(editedMenuItem) && model.hasMenuItem(editedMenuItem)) {
+            throw new CommandException(MESSAGE_DUPLICATE_MENU_ITEM);
+        }
+
+        model.setMenuItem(menuItemToEdit, editedMenuItem);
+        return new CommandResult(String.format(MESSAGE_EDIT_MENU_ITEM_SUCCESS,
+                editedMenuItem.getName().fullName,
+                editedMenuItem.getPrice().value,
+                editedMenuItem.isAvailable()));
+    }
+
+    /**
+     * Creates and returns a {@code MenuItem} with the details of {@code menuItemToEdit}
+     * edited with {@code editMenuDescriptor}.
+     */
+    private static MenuItem createEditedMenuItem(MenuItem menuItemToEdit,
+                                                  EditMenuDescriptor editMenuDescriptor) {
+        assert menuItemToEdit != null;
+
+        MenuItemName updatedName = editMenuDescriptor.getName().orElse(menuItemToEdit.getName());
+        Price updatedPrice = editMenuDescriptor.getPrice().orElse(menuItemToEdit.getPrice());
+        boolean updatedAvailable = editMenuDescriptor.getAvailable().orElse(menuItemToEdit.isAvailable());
+        return new MenuItem(updatedName, updatedPrice, updatedAvailable);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof EditMenuCommand)) {
+            return false;
+        }
+
+        EditMenuCommand otherCommand = (EditMenuCommand) other;
+        return index.equals(otherCommand.index)
+                && editMenuDescriptor.equals(otherCommand.editMenuDescriptor);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("index", index)
+                .add("editMenuDescriptor", editMenuDescriptor)
+                .toString();
+    }
+
+    /**
+     * Stores the details to edit the menu item with.
+     * Each non-empty field value will replace the corresponding field value of the menu item.
+     */
+    public static class EditMenuDescriptor {
+        private MenuItemName name;
+        private Price price;
+        private Boolean available;
+
+        /**
+         * Creates an empty {@code EditMenuDescriptor}.
+         */
+        public EditMenuDescriptor() {}
+
+        /**
+         * Copy constructor.
+         */
+        public EditMenuDescriptor(EditMenuDescriptor toCopy) {
+            setName(toCopy.name);
+            setPrice(toCopy.price);
+            setAvailable(toCopy.available);
+        }
+
+        /**
+         * Returns true if at least one field is edited.
+         */
+        public boolean isAnyFieldEdited() {
+            return CollectionUtil.isAnyNonNull(name, price, available);
+        }
+
+        /**
+         * Sets the name to edit.
+         */
+        public void setName(MenuItemName name) {
+            this.name = name;
+        }
+
+        /**
+         * Returns the name to edit, if any.
+         */
+        public Optional<MenuItemName> getName() {
+            return Optional.ofNullable(name);
+        }
+
+        /**
+         * Sets the price to edit.
+         */
+        public void setPrice(Price price) {
+            this.price = price;
+        }
+
+        /**
+         * Returns the price to edit, if any.
+         */
+        public Optional<Price> getPrice() {
+            return Optional.ofNullable(price);
+        }
+
+        /**
+         * Sets the availability to edit.
+         */
+        public void setAvailable(Boolean available) {
+            this.available = available;
+        }
+
+        /**
+         * Returns the availability to edit, if any.
+         */
+        public Optional<Boolean> getAvailable() {
+            return Optional.ofNullable(available);
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            if (other == this) {
+                return true;
+            }
+
+            // instanceof handles nulls
+            if (!(other instanceof EditMenuDescriptor)) {
+                return false;
+            }
+
+            EditMenuDescriptor otherDesc = (EditMenuDescriptor) other;
+            return Objects.equals(name, otherDesc.name)
+                    && Objects.equals(price, otherDesc.price)
+                    && Objects.equals(available, otherDesc.available);
+        }
+
+        @Override
+        public String toString() {
+            return new ToStringBuilder(this)
+                    .add("name", name)
+                    .add("price", price)
+                    .add("available", available)
+                    .toString();
+        }
+    }
+}
---
+++ src/main/java/seedu/homechef/logic/parser/EditMenuCommandParser.java
@@ -0,0 +1,61 @@
+package seedu.homechef.logic.parser;
+
+import static seedu.homechef.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.homechef.logic.parser.CliSyntax.PREFIX_AVAILABILITY;
+import static seedu.homechef.logic.parser.CliSyntax.PREFIX_MENU_NAME;
+import static seedu.homechef.logic.parser.CliSyntax.PREFIX_PRICE;
+
+import seedu.homechef.commons.core.index.Index;
+import seedu.homechef.logic.commands.EditMenuCommand;
+import seedu.homechef.logic.commands.EditMenuCommand.EditMenuDescriptor;
+import seedu.homechef.logic.parser.exceptions.ParseException;
+import seedu.homechef.model.menu.MenuItemName;
+import seedu.homechef.model.menu.Price;
+
+/**
+ * Parses input arguments and creates a new EditMenuCommand object.
+ */
+public class EditMenuCommandParser implements Parser<EditMenuCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments and returns an EditMenuCommand object.
+     *
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public EditMenuCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_MENU_NAME, PREFIX_PRICE, PREFIX_AVAILABILITY);
+
+        Index index;
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (ParseException pe) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    EditMenuCommand.MESSAGE_USAGE), pe);
+        }
+
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_MENU_NAME, PREFIX_PRICE, PREFIX_AVAILABILITY);
+
+        EditMenuDescriptor editMenuDescriptor = new EditMenuDescriptor();
+
+        if (argMultimap.getValue(PREFIX_MENU_NAME).isPresent()) {
+            editMenuDescriptor.setName(new MenuItemName(argMultimap.getValue(PREFIX_MENU_NAME).get().trim()));
+        }
+        if (argMultimap.getValue(PREFIX_PRICE).isPresent()) {
+            editMenuDescriptor.setPrice(new Price(argMultimap.getValue(PREFIX_PRICE).get().trim()));
+        }
+        if (argMultimap.getValue(PREFIX_AVAILABILITY).isPresent()) {
+            String availStr = argMultimap.getValue(PREFIX_AVAILABILITY).get().trim().toLowerCase();
+            if (!availStr.equals("true") && !availStr.equals("false")) {
+                throw new ParseException("Availability must be 'true' or 'false'.");
+            }
+            editMenuDescriptor.setAvailable(availStr.equals("true"));
+        }
+
+        if (!editMenuDescriptor.isAnyFieldEdited()) {
+            throw new ParseException(EditMenuCommand.MESSAGE_NOT_EDITED);
+        }
+
+        return new EditMenuCommand(index, editMenuDescriptor);
+    }
+}
